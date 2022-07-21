47c2614464d05c631fc0b3f853b1c810fe2529fb
Suelem Macedo <suelemmacedo_@hotmail.com>
2022-07-21 11:51:59 -0300

    Exercício 1 do Bloco 4

---
+++ fundamentos/b4-JavaScript/exercicio-b1-dia4.1.js
@@ -0,0 +1,12 @@
+const myName = "Suelem";
+const birthCity = "Bananeiras";
+let birthYear = 1991;
+
+birthYear = 2030;
+birthCity = "Campina"
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+console.log(birthYear);
