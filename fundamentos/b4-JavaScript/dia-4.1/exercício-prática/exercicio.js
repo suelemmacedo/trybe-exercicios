ad7461a478eed07f4bbd513d69cb258fcfa2ce6d
Suelem Macedo <suelemmacedo_@hotmail.com>
2022-07-22 12:02:07 -0300

    Exercício Prático Bloco 1

---
+++ fundamentos/b4-JavaScript/dia-4.1/exercício-prática/exercicio.js
@@ -0,0 +1,45 @@
+// Exercício Prática //
+// 1 //
+
+const A = 10;
+const B = 13;
+
+Adição = ( A +  B);
+console.log (Adição);
+
+Subtração = (A - B);
+console.log (Subtração);
+
+Multiplicação = (A * B);
+console.log (Multiplicação);
+
+Divisão = (A / B);
+console.log (Divisão);
+
+Módulo = (A % B);
+console.log (Módulo);
+
+// 2 //
+
+const numero1 = 15;
+const numero2 = 13;
+
+if ( numero1 > numero2)  {
+    console.log ("Número 1 é maior que número 2");
+} else {
+console.log ("Número 1 é menor que número 2");
+}
+
+// 3 //
+
+const C = 35;
+const D = 105;
+const E = 58;
+
+if (C > D && C > E) {
+    console.log ("O maior número é C");
+} else if (D > C && D > E) {
+    console.log ("O maior número é D");
+} else {
+    console.log ("O maior número é E");
+}
