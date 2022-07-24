f2f0cd5dc8b35e5584550e58ba87fda98e1c2ebc
Suelem Macedo <suelemmacedo_@hotmail.com>
2022-07-24 10:01:29 -0300

    update da questao 9

--- fundamentos/b4-JavaScript/dia-4.1/exercício-prática/exercicio.js
+++ fundamentos/b4-JavaScript/dia-4.1/exercício-prática/exercicio.js
@@ -109,3 +109,16 @@ if (number1 % 2===0 || number2 % 2===0 || number3 % 2===0) {
 } else {
   console.log("false");
 }
+
+// 9 Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false.
+// BÔnus: use somente um if
+
+const num1 = 14;
+const num2 = 10;
+const num3 = 20;
+
+if (num1 % 2!==0 || num2 % 2!==0 | num3 % 2!==0) {
+  console.log("true");
+} else {
+  console.log("false");
+}
